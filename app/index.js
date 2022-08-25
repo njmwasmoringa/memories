e3317835c7733997742394a2fbeb713c48b73d9b
njmwasmoringa <julius.mwangi@moringaschool.com>
2022-08-25 17:33:37 +0300

    Posting and retrieving memories. Integrate bootstrap

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 
 let memoriesImage;
+const apiHost = "http://localhost:3000";
+
 function onMemoriesFormSubmit(evt) {
     evt.preventDefault();
 
@@ -21,8 +23,8 @@ function onMemoriesFormSubmit(evt) {
         }
     }
 
-    fetch( "http://localhost:3000/memories", fetchParameters ).then( (response)=>{
-        return response
+    fetch( `${apiHost}/memories`, fetchParameters ).then( (response)=>{
+        getAndLoadMemories();
     } );
 
     debugger
@@ -38,9 +40,48 @@ function getImage(evt) {
     fileReader.readAsDataURL(file);
 }
 
+function getAndLoadMemories(){
+
+    // Clear the status before the request
+    document.getElementById("status").innerHTML = ``;
+
+    // Request for the memories
+    fetch( `${apiHost}/memories` )
+    .then( resp=>{
+        if(resp.status != 200){
+            throw new Error( resp.statusText );
+        }
+        return resp.json();
+    } )
+    .then( memories=>{
+        
+        const memoriesHtmlContent = memories
+        .sort((a, b)=>b.id - a.id)
+        .map( cardHtml ).join('');
+
+        document.getElementById('postedMemories').innerHTML = memoriesHtmlContent;
+    } )
+    .catch( error=>{
+        document.getElementById("status").innerHTML = `Error: ${error.message}`;
+    } );
+}
+
+function cardHtml( memory ){
+    return `
+    <div class="card mb-3">
+        <img src="${memory.memoriesImage}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text">${memory.memoriesText}</p>
+        </div>
+    </div>
+    `;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const memoriesForm = document.querySelector('#memoriesForm');
     memoriesForm.addEventListener('submit', onMemoriesFormSubmit);
+    document.getElementById('image').addEventListener('change', getImage);
 
+    getAndLoadMemories();
 });
